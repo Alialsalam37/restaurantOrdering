8a56cc10c060fa954c9066e6f83854d7931cadb4
Alialsalam37 <alisalam65420@gmail.com>
2026-01-28 08:30:16 +0300

    update index.js

--- index.js
+++ index.js
@@ -1,59 +1,59 @@
-import {menuArray} from '/data.js'
+import { menuArray } from '/data.js'
 
 const content = document.getElementById("content")
 
 const priceArr = []
 
-document.addEventListener('click',function(e){
-    if(e.target.dataset.add){
+document.addEventListener('click', function (e) {
+    if (e.target.dataset.add) {
         handelAddClick(e.target.dataset.add)
         document.getElementById('footerDiv').style.display = 'block'
     }
-    else if(e.target.dataset.remove){
+    else if (e.target.dataset.remove) {
         handelRemoveClick(e.target.dataset.remove)
     }
-    else if(e.target.id === "completeBtn"){
-        document.getElementById('cardDetails').style.display= 'block'
+    else if (e.target.id === "completeBtn") {
+        document.getElementById('cardDetails').style.display = 'block'
     }
-    else if(e.target.id === "payBtn"){
+    else if (e.target.id === "payBtn") {
         e.preventDefault()
         document.getElementById('footerDiv').style.display = 'none'
-        document.getElementById('cardDetails').style.display= 'none'
-        document.getElementById("finishOrder").style.display= 'block'
-        
+        document.getElementById('cardDetails').style.display = 'none'
+        document.getElementById("finishOrder").style.display = 'block'
+
     }
-    
+
 })
 
-function handelAddClick(mealId){
-    const targetMeal = menuArray.filter(meal => meal.id === Number(mealId) )[0]
+function handelAddClick(mealId) {
+    const targetMeal = menuArray.filter(meal => meal.id === Number(mealId))[0]
     renderAddClick(targetMeal)
 }
-function renderAddClick(meal){
+function renderAddClick(meal) {
     priceArr.push(meal.price)
-    document.getElementById("renderOrder").innerHTML +=`
+    document.getElementById("renderOrder").innerHTML += `
     <div class="order" id="${meal.id}">
         <h6 class="orderName">${meal.name}</h6>
         <button class="removeBtn" id="removeBtn" data-remove=${meal.id}>remove</button>
         <h6 class="orderPrice">$${meal.price}</h6>
     </div>
     `
-    document.getElementById("orderPrice").textContent=`$${priceArr.reduce( (total,first)=>total+first)}`
+    document.getElementById("orderPrice").textContent = `$${priceArr.reduce((total, first) => total + first, 0)}`
 }
 
-function handelRemoveClick(mealId){
+function handelRemoveClick(mealId) {
     document.getElementById(mealId).remove()
-    
-    const targetMealRemove = menuArray.filter(remove=> remove.id === Number(mealId))[0]
+
+    const targetMealRemove = menuArray.filter(remove => remove.id === Number(mealId))[0]
     const removePrice = targetMealRemove.price
-    
+
     priceArr.splice(priceArr.indexOf(removePrice), 1)
-    
-    document.getElementById("orderPrice").textContent=`$${priceArr.reduce( (total,first)=>total+first)}` 
-    
+
+    document.getElementById("orderPrice").textContent = `$${priceArr.reduce((total, first) => total + first, 0)}`
+
 }
 
-function getMenuRender(){
+function getMenuRender() {
     const menuHTML = menuArray.map(item => {
         return `
         <div class="container">
